1452ead55ae88608490449514d1eea62c830beb7
Griffen Morrison <griffenmorrison@yahoo.com>
2022-03-24 16:40:44 -0500

    more visual attributes

--- ParticleExperiment/app/src/main/java/com/cphandheld/particleexperiment/MainActivity.kt
+++ ParticleExperiment/app/src/main/java/com/cphandheld/particleexperiment/MainActivity.kt
@@ -3,6 +3,8 @@ package com.cphandheld.particleexperiment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- ParticleExperiment/app/src/main/java/com/cphandheld/particleexperiment/ParticleView.kt
+++ ParticleExperiment/app/src/main/java/com/cphandheld/particleexperiment/ParticleView.kt
@@ -15,6 +15,12 @@ class ParticleView @JvmOverloads constructor(context: Context, attrs: AttributeS
 
     private val circlePaint: Paint = Paint()
     private val linePaint: Paint = Paint()
+    private var particleVariance: Float
+    private var particleAlphaMin: Float? = null
+    private var lineDistance: Float
+    private val maxSpeed: Float
+    private val minSpeed: Float
+    private val strongLineConnection: Boolean
     private val particleManager: ParticleManager = ParticleManager()
     private val mainHandler = Handler(Looper.getMainLooper())
     private var isRunning = false
@@ -31,7 +37,23 @@ class ParticleView @JvmOverloads constructor(context: Context, attrs: AttributeS
                 particleManager.setParticleRadius(getDimension(R.styleable.ParticleView_particleRadius, 10f))
                 circlePaint.color = getColor(R.styleable.ParticleView_particleColor, Color.WHITE)
                 linePaint.color = getColor(R.styleable.ParticleView_lineColor, Color.WHITE)
-                linePaint.strokeWidth = getDimension(R.styleable.ParticleView_lineThickness, 10f)
+                linePaint.strokeWidth = getDimension(R.styleable.ParticleView_lineThickness, 1f)
+                lineDistance = getDimension(R.styleable.ParticleView_lineDistance, 100f)
+                maxSpeed = getFloat(R.styleable.ParticleView_maxSpeed, 10f)
+                minSpeed = getFloat(R.styleable.ParticleView_minSpeed, 1f)
+                strongLineConnection = getBoolean(R.styleable.ParticleView_strongLineConnection, false)
+
+                particleVariance = getFloat(R.styleable.ParticleView_particleRadiusVariance, 0f)
+
+                val gottenAlphaMin = getFloat(R.styleable.ParticleView_alphaMin, -1f)
+                if (gottenAlphaMin > 0) {
+                    particleAlphaMin = if (gottenAlphaMin > 1f) {
+                        1f
+                    } else {
+                        gottenAlphaMin
+                    }
+                }
+
             } finally {
                 recycle()
             }
@@ -41,7 +63,7 @@ class ParticleView @JvmOverloads constructor(context: Context, attrs: AttributeS
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         particleManager.setBounds(width.toFloat(), height.toFloat())
-        particleManager.initialize()
+        particleManager.initialize(particleVariance, particleAlphaMin, minSpeed, maxSpeed)
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -50,11 +72,16 @@ class ParticleView @JvmOverloads constructor(context: Context, attrs: AttributeS
         particleManager.particles.forEach { particle ->
             particleManager.particles.forEach { otherParticle ->
                 val dist = sqrt((otherParticle.yPos - particle.yPos) * (otherParticle.yPos - particle.yPos) + (otherParticle.xPos - particle.xPos) * (otherParticle.xPos - particle.xPos))
-                if (dist < 100f.px) {
+                if (dist < lineDistance.dpToPx) {
+                    if (!strongLineConnection) {
+                        val strength = ((1f - dist / lineDistance.dpToPx) * 255).toInt()
+                        linePaint.alpha = strength
+                    }
                     canvas.drawLine(particle.xPos, particle.yPos, otherParticle.xPos, otherParticle.yPos, linePaint)
                 }
             }
-            canvas.drawCircle(particle.xPos, particle.yPos, particle.radius, circlePaint)
+            circlePaint.alpha = particle.alpha ?: 255
+            canvas.drawCircle(particle.xPos, particle.yPos, particle.radius.dpToPx, circlePaint)
         }
     }
 
@@ -82,6 +109,7 @@ class ParticleView @JvmOverloads constructor(context: Context, attrs: AttributeS
             }
         }, 10)
     }
+
 
 }
 
@@ -116,21 +144,29 @@ class ParticleManager {
         }
     }
 
-    fun initialize(radiusVariance: Float? = null) {
-
-        radiusVariance?.let {
-            //TODO Variation in particle size
-        }
+    fun initialize(radiusVariance: Float, alphaMin: Float? = null, maxSpeed: Float, minSpeed: Float) {
 
         for (i in 0 until particleCount) {
-            val particle = Particle(radius)
+            val variance = radius * radiusVariance
+            val radiusMin = radius - variance
+            val radiusMax = radius + variance
+            val calcRadius = radiusMin + Math.random().toFloat() * (radiusMax - radiusMin)
+
+            val alpha: Int? = ((alphaMin?.plus(Math.random().toFloat() * 1f))?.times(255))?.toInt()
+
+            val particle = Particle(calcRadius, alpha, maxSpeed, minSpeed)
             particle.xPos = Math.random().toFloat() * width
             particle.yPos = Math.random().toFloat() * height
             particles.add(particle)
         }
     }
 
-    class Particle(val radius: Float) {
+    class Particle(
+        val radius: Float,
+        val alpha: Int? = null,
+        minSpeed: Float = 1f,
+        maxSpeed: Float = 10f
+    ) {
 
         enum class Sides {
             Top, Right, Bottom, Left
@@ -140,7 +176,7 @@ class ParticleManager {
         var yPos: Float = 0f
         var xVel: Float = (-1 + Math.random() * 2).toFloat()
         var yVel: Float = (-1 + Math.random() * 2).toFloat()
-        private val speed = 10f
+        private val speed = (minSpeed + Math.random() * maxSpeed).toFloat()
 
         fun move() {
             xPos += xVel * speed
@@ -188,7 +224,7 @@ class ParticleManager {
 
 }
 
-val Float.dp: Float
+val Float.pxToDp: Float
     get() = (this / Resources.getSystem().displayMetrics.density)
-val Float.px: Float
+val Float.dpToPx: Float
     get() = (this * Resources.getSystem().displayMetrics.density)
